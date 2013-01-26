651f546588f8fe6e514aa23268049a80353c3987
matt <matt@cantab.net>
2013-01-26 14:27:25 +0000

    Have problem_gen always spit out anagrams

--- problem_gen.py
+++ problem_gen.py
@@ -5,10 +5,23 @@ import random
 
 chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-max_letters = int(sys.argv[1])
-num_questions = int(sys.argv[2])
+wordlist = open(sys.argv[1])
+max_letters = int(sys.argv[2])
+num_questions = int(sys.argv[3])
+
+ws = []
+
+for w in wordlist.xreadlines():
+  w = w.strip()
+
+  if len(w) == max_letters:
+    ws.append(w)
+
+wordlist.close()
 
 for i in xrange(num_questions):
-  s = ''.join(random.choice(chars) for j in xrange(max_letters))
-  n = random.randint(1, max_letters)
+  s = list(random.choice(ws))
+  random.shuffle(s)
+  s = ''.join(s)
+  n = random.randint(2, max_letters)
   print "%s %d" % (s, n)
